da95cbc3ae5e828459b41149d6a4cf3539aec0b9
Du Yizhuo <dyzdyz010@sina.com>
2016-04-21 09:48:02 +0800

    Refactor

---
+++ BorderPoliceNews/BorderPoliceNews/PaperViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PaperViewController.swift
+//  BorderPoliceNews
+//
+//  Created by Du Yizhuo on 16/4/18.
+//  Copyright © 2016年 MLTT. All rights reserved.
+//
+
+
+class PaperViewController: UIViewController {
+
+}
